ef4315be4b53b5c9342950e51e8d5570622708a3
Vanessa Felix Christoff <vfchristoff9@gmail.com>
2023-05-04 22:19:00 -0300

    Light and Dark mode funcionality and Topbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
+import Topbar from "./scenes/global/Topbar";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+
+
 function App() {
-  return <div className="App">App</div>;
-  
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar/>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
-export default App
+export default App;
