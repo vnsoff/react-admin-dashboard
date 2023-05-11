a181df99e07a92f1a3b771c757e18b49413bfc16
Vanessa Felix Christoff <vfchristoff9@gmail.com>
2023-05-11 01:12:29 -0300

    Form to add New User - Fix

--- src/scenes/Profile/index.jsx
+++ src/scenes/Profile/index.jsx
@@ -31,7 +31,7 @@ const Form = () => {
           <form onSubmit={handleSubmit}>
             <Box
               display="grid"
-              gap="20px 90px"
+              gap="20px 50px"
               gridTemplateColumns="repeat(4, minmax(0, 1fr))"
               sx={{
                 "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
